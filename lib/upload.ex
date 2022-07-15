1f99b2816f4eba452079ddf91539d1f1b6eb92fe
5HT <maxim@synrc.com>
2022-07-15 07:34:23 +0300

    wip

--- lib/upload.ex
+++ lib/upload.ex
@@ -21,34 +21,43 @@ defmodule CIPHER.UP do
   end
 
   def proc(:init, N2O.pi(state: {login, pass, from, to, doc, mode}) = pi) do
-
-    {:ok, file} = :file.read_file(doc)
-    url = 'https://ccs-dev-api.cipher.kiev.ua/ccs/auth-2/token'
-    body = :jsone.encode([grant_type: "password", username: login, client_id: "arch-client", password: pass])
-    len = :io_lib.format('~p',[:erlang.size(body)])
-    file_len = :io_lib.format('~p',[:erlang.size(file)])
-    app_json = 'application/json'
-    headers = [{'Content-Type',app_json},{'Content-Length', len}]
-    {:ok,{status,headers,body}} = :httpc.request(:post, {url, headers, app_json, body}, [{:timeout,10000}], [{:body_format,:binary}])
-    res = :jsone.decode body
-    bearer = :maps.get "token_type", res
-    token = :maps.get "access_token", res
-    tok = bearer <> " " <> token |> :erlang.binary_to_list
-    url = 'https://archive-api.cipher.com.ua/arch/api/v1/object/1'
-    octet = 'application/octet-stream'
-    headers = [{'Authorization',tok},{'Content-Type',octet},{'Content-Length', file_len}]
-    {:ok,{status,headers,body}} = :httpc.request(:post, {url, headers, octet, file}, [{:timeout,100000}], [{:body_format,:binary}])
-    CIPHER.debug 'STATUS: ~p~n', [status]
-    cancel(doc)
-    {:ok, N2O.pi(pi, state: {login, token, from, to, doc, mode})}
+      bearer = auth(login, pass)
+      res = upload(bearer, doc)
+      CIPHER.debug 'STATUS: ~p~n', [res]
+      cancel(doc)
+      {:ok, N2O.pi(pi, state: {login, pass, from, to, doc, mode})}
   end
 
   def proc({:upload, messageSize, pos, count, sessionId, rest} = m,
         N2O.pi(state: {login, pass, _, _, msg_id, _}) = pi) do
-    {:noreply, pi}
+      {:noreply, pi}
   end
 
   def proc(any) do
+  end
+
+  def upload(bearer,doc) do
+      {:ok, file} = :file.read_file(doc)
+      file_len = :io_lib.format('~p',[:erlang.size(file)])
+      url = :application.get_env(:n2o, :cipher_upload, [])
+      octet = 'application/octet-stream'
+      headers = [{'Authorization',bearer},{'Content-Type',octet},{'Content-Length', file_len}]
+      {:ok,{status,headers,body}} = :httpc.request(:post, {url, headers, octet, file}, [{:timeout,100000}], [{:body_format,:binary}])
+      CIPHER.debug 'STATUS: ~p~n', [status]
+      :jsone.decode body
+  end
+
+  def auth(login,pass) do
+      url = :application.get_env(:n2o, :cipher_auth, [])
+      body = :jsone.encode([grant_type: "password", username: login, client_id: "arch-client", password: pass])
+      len = :io_lib.format('~p',[:erlang.size(body)])
+      app_json = 'application/json'
+      headers = [{'Content-Type',app_json},{'Content-Length', len}]
+      {:ok,{status,headers,body}} = :httpc.request(:post, {url, headers, app_json, body}, [{:timeout,10000}], [{:body_format,:binary}])
+      res = :jsone.decode body
+      bearer = :maps.get "token_type", res
+      token = :maps.get "access_token", res
+      tok = bearer <> " " <> token |> :erlang.binary_to_list
   end
 
 end
